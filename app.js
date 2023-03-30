5f7d2d012b7cfb422d83366402697e62e7de4bfd
Sajib Das Rinku <sdrinku13@gmail.com>
2023-03-31 01:36:54 +0600

    2nd

---
+++ app.js
@@ -0,0 +1,25 @@
+// var firstName =["hdd"];
+
+// console.log(firstName);
+// console.log(typeof firstName);
+
+// parseFloat/parseInt
+
+var num1 = 10;
+var num2 = "20";
+
+const convertnum2 =parseFloat(num2);
+
+
+// console.log(typeof num1);
+// console.log(typeof num2);
+console.log( typeof convertnum2);
+
+// const sum = num1 +convertnum2 ;
+  const sum = num1 * num2; 
+// in the matter of multiplication it works without converting string to number   
+
+
+
+console.log(sum);
+
