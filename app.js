1220aefec59834398d61ec73784d81b8ee7c384d
Sajib Das Rinku <sdrinku13@gmail.com>
2023-04-02 00:23:51 +0600

    3rd

--- app.js
+++ app.js
@@ -1,25 +1,61 @@
-// var firstName =["hdd"];
+// // var firstName =["hdd"];
 
-// console.log(firstName);
-// console.log(typeof firstName);
+// // console.log(firstName);
+// // console.log(typeof firstName);
 
-// parseFloat/parseInt
+// // parseFloat/parseInt
 
-var num1 = 10;
-var num2 = "20";
+// var num1 = 10;
+// var num2 = "20";
 
-const convertnum2 =parseFloat(num2);
-
-
-// console.log(typeof num1);
-// console.log(typeof num2);
-console.log( typeof convertnum2);
-
-// const sum = num1 +convertnum2 ;
-  const sum = num1 * num2; 
-// in the matter of multiplication it works without converting string to number   
+// const convertnum2 =parseFloat(num2);
 
 
+// // console.log(typeof num1);
+// // console.log(typeof num2);
+// console.log( typeof convertnum2);
 
-console.log(sum);
+// // const sum = num1 +convertnum2 ;
+//   const sum = num1 * num2; 
+// // in the matter of multiplication it works without converting string to number   
+// console.log(sum);
 
+                        // // If/else
+
+
+
+
+    // var name ="rinku";
+    // if (name =="rinku"){
+    //     console.log("this is rinku");
+    // }
+    // else if (name=="tinku"){
+    //     console.log("this is tinku");
+    // }
+    // else{
+    //     console.log("no one");
+    //  }
+
+
+     var num = 50;
+     if (num==20){
+        console.log("its 20");
+     }
+     else if(num==30){
+        console.log("its 30")
+     }
+     else{
+        console.log("its nothing")
+     }
+
+
+     var myRoll= 13;
+     if (myRoll==12){
+console.log("this is not my roll");
+     }
+     else if (myRoll==13){
+      console.log("this is my role");
+     }
+     else{
+      console.log("there is not my roll");
+     }
