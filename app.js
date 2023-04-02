d121ea48cd55bfbf98d22733009debf57e5d5166
Sajib Das Rinku <sdrinku13@gmail.com>
2023-04-03 00:41:54 +0600

    4th

--- app.js
+++ app.js
@@ -1,61 +1,101 @@
-// // var firstName =["hdd"];
+// // // // var firstName =["hdd"];
 
-// // console.log(firstName);
-// // console.log(typeof firstName);
+// // // // console.log(firstName);
+// // // // console.log(typeof firstName);
 
-// // parseFloat/parseInt
+// // // // parseFloat/parseInt
+
+// // // var num1 = 10;
+// // // var num2 = "20";
+
+// // // const convertnum2 =parseFloat(num2);
+
+
+// // // // console.log(typeof num1);
+// // // // console.log(typeof num2);
+// // // console.log( typeof convertnum2);
+
+// // // // const sum = num1 +convertnum2 ;
+// // //   const sum = num1 * num2; 
+// // // // in the matter of multiplication it works without converting string to number   
+// // // console.log(sum);
+
+// //                         // // If/else
+
+
+
+
+// //     // var name ="rinku";
+// //     // if (name =="rinku"){
+// //     //     console.log("this is rinku");
+// //     // }
+// //     // else if (name=="tinku"){
+// //     //     console.log("this is tinku");
+// //     // }
+// //     // else{
+// //     //     console.log("no one");
+// //     //  }
+
+
+// //      var num = 50;
+// //      if (num==20){
+// //         console.log("its 20");
+// //      }
+// //      else if(num==30){
+// //         console.log("its 30")
+// //      }
+// //      else{
+// //         console.log("its nothing")
+// //      }
+
+
+// //      var myRoll= 13;
+// //      if (myRoll==12){
+// // console.log("this is not my roll");
+// //      }
+// //      else if (myRoll==13){
+// //       console.log("this is my role");
+// //      }
+// //      else{
+// //       console.log("there is not my roll");
+// //      }
+
+
+// // var num3 =14;
+// // var num4 =31;
+
+// // if (num3==13 && num4==31){
+// //    console.log(" its true for both");
+// // }
+// // else if (num3==13 || num4==30){
+// //    console.log(" its half true");
+// // }
+// // else{
+// //    console.log("there is no true");
+// // }
+
 
 // var num1 = 10;
-// var num2 = "20";
+// var num2 = 20;
+// var num3 = 30;
 
-// const convertnum2 =parseFloat(num2);
 
-
-// // console.log(typeof num1);
-// // console.log(typeof num2);
-// console.log( typeof convertnum2);
-
-// // const sum = num1 +convertnum2 ;
-//   const sum = num1 * num2; 
-// // in the matter of multiplication it works without converting string to number   
-// console.log(sum);
-
-                        // // If/else
+// if (num1>num2 && num1>num3){
+//    console.log(" num1 is bigger")
+// }
+// else if (num2>num3){
+//    console.log("num2 is bigger ")
+// }
+// else{
+//    console.log(" num3 is bigger")
+// }
 
 
 
+               // array
 
-    // var name ="rinku";
-    // if (name =="rinku"){
-    //     console.log("this is rinku");
-    // }
-    // else if (name=="tinku"){
-    //     console.log("this is tinku");
-    // }
-    // else{
-    //     console.log("no one");
-    //  }
+ var numbers = [1,"rinku",3,"tinku",5,6,7,"jinku",9,10];
+ console.log(numbers.length);
 
+ 
 
-     var num = 50;
-     if (num==20){
-        console.log("its 20");
-     }
-     else if(num==30){
-        console.log("its 30")
-     }
-     else{
-        console.log("its nothing")
-     }
-
-
-     var myRoll= 13;
-     if (myRoll==12){
-console.log("this is not my roll");
-     }
-     else if (myRoll==13){
-      console.log("this is my role");
-     }
-     else{
-      console.log("there is not my roll");
-     }
